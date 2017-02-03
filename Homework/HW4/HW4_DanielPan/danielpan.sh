f1839a9fe38aad6a965c7e19a23c1e37bf5139bd
Tai-Yu Pan <taiyupan@uw.edu>
2017-02-03 04:52:20 -0800

    fix name errors

--- Homework/HW4/HW4_DanielPan/danielpan.sh
+++ Homework/HW4/HW4_DanielPan/danielpan.sh
@@ -5,7 +5,7 @@ dimensions=(300 350 400)
 
 for (( i=0 ; i<=2 ; i++ )) ; do
 	ctemp=${dimensions[$i]}
-	cp input.inp ${name}.inp
-	sed -i "8s/.*/Temp=${ctemp}/" ${name}.inp
-	source master.sh ${name} > ${name}.log &
+	cp input.inp ${name[$i]}.inp
+	sed -i "8s/.*/Temp=${ctemp}/" ${name[$i]}.inp
+	source master.sh ${name[$i]} > ${name[$i]}.log &
 done
