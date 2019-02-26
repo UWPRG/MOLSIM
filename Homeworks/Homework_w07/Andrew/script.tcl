8e6cd9b084bcb54df04798e6c6bdcfb0f6bc535c
Andrew Kullman <38169014+akullmanUW@users.noreply.github.com>
2019-02-25 18:12:18 -0800

    HomeworkFolder

---
+++ Homeworks/Homework_w07/Andrew/script.tcl
@@ -0,0 +1,12 @@
+# VMD for WIN32, version 1.9.3 (November 30, 2016)
+# Log file 'C:/Users/a97Dr/Downloads/script', created by user a97Dr
+menu graphics off
+menu graphics on
+mol new {C:/Users/a97Dr/Downloads/1ej1.pdb} type {pdb} first 0 last -1 step 1 waitfor 1
+mol modcolor 0 0 Structure
+mol modstyle 0 0 NewCartoon 0.300000 10.000000 4.100000 0
+mol modmaterial 0 0 Glossy
+mol modselect 0 0 not waters
+menu graphics off
+# VMD for WIN32, version 1.9.3 (November 30, 2016)
+# end of log file.
