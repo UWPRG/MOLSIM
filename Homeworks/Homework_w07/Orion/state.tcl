2160bfdc00a959ad48498ec1ca8dad5027d073f8
Orion Dollar <oriondollar@D-10-19-33-187.dhcp4.washington.edu>
2019-03-08 12:35:45 -0800

    Orion HW

---
+++ Homeworks/Homework_w07/Orion/state.tcl
@@ -0,0 +1,20 @@
+mol modselect 0 0 all water
+mol modcolor 0 0 Element
+mol color Element
+mol modstyle 0 0 HBonds 2.900000 20.000000 1.000000
+mol modmaterial 0 0 Transparent
+mol selection all water
+mol material Transparent
+mol addrep 0
+mol modselect 1 0 all protein
+mol modcolor 1 0 Chain
+mol modstyle 1 0 NewCartoon 0.300000 10.000000 4.100000 0
+mol color Chain
+mol representation NewCartoon 0.300000 10.000000 4.100000 0
+mol selection all protein
+mol material AOChalky
+mol addrep 0
+mol modselect 2 0 all resname QUA
+mol modstyle 2 0 VDW 1.000000 12.000000
+mol modcolor 2 0 Name
+mol modstyle 2 0 VDW 0.500000 12.000000
